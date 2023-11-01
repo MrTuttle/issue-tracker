8e3ab267e5a414bc118e12d3b3a02b9c07889305
Florent <florent.vincerot@me.com>
2023-11-01 18:33:25 +0100

    building IssueSummary comp

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,26 @@
 
 import Image from "next/image";
 import LatestIssues from "./LatestIssues";
+import IssueSummary from "./IssueSummary";
+import prisma from "@/prisma/client";
+import { closeSync } from "fs";
 
-export default function Home() {
+export default async function Home() {
+  const open = await prisma.issue.count({
+    where: { status: "OPEN" },
+  });
+  const closed = await prisma.issue.count({
+    where: { status: "CLOSED" },
+  });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+
   return (
     <>
       <div>Home page</div>
       <LatestIssues />
+      <IssueSummary open={open} closed={closed} inProgress={inProgress} />
     </>
   );
 }
